1007e3e6c278f85ed4589173f52bd8773880575f
Ayman Chaouki <chaoukiayman2@gmail.com>
2025-06-02 00:23:52 +0200

    Adding a mountain car DQN agent

--- deep_rl/agents.py
+++ deep_rl/agents.py
@@ -1,5 +1,5 @@
 from dqn import DQN
-from networks import LunarLanderNetwork
+from networks import LunarLanderNetwork, CartPoleNetwork, MountainCarNetwork
 
 class LunarLanderDQN(DQN):
     """
@@ -23,3 +23,49 @@ class LunarLanderDQN(DQN):
 
         super(LunarLanderDQN, self).__init__(env, gamma, max_size)
         self.q_network = LunarLanderNetwork()
+
+class CartPoleDQN(DQN):
+    """
+    Description
+    --------------
+    Class describing an DQN LunarLander agent.
+    """
+
+    def __init__(self, env, gamma=0.99, max_size=500000):
+        """
+        Description
+        --------------
+        Constructor of class FrozenLakeAgent.
+        
+        Arguments
+        --------------
+        env      : LunarLander-v3 gymnasium environment.
+        gamma    : Float in [0, 1] generally close to 1, discount factor.
+        max_size : Int, maximum size of the replay buffer.
+        """
+
+        super(CartPoleDQN, self).__init__(env, gamma, max_size)
+        self.q_network = CartPoleNetwork()
+
+class MountainCarDQN(DQN):
+    """
+    Description
+    --------------
+    Class describing an DQN LunarLander agent.
+    """
+
+    def __init__(self, env, gamma=0.99, max_size=500000):
+        """
+        Description
+        --------------
+        Constructor of class FrozenLakeAgent.
+        
+        Arguments
+        --------------
+        env      : LunarLander-v3 gymnasium environment.
+        gamma    : Float in [0, 1] generally close to 1, discount factor.
+        max_size : Int, maximum size of the replay buffer.
+        """
+
+        super(MountainCarDQN, self).__init__(env, gamma, max_size)
+        self.q_network = MountainCarNetwork()
