a165ad0192465de99ffbc209cadd30dbc4238b5d
Ayman Chaouki <chaoukiayman2@gmail.com>
2026-01-31 18:35:01 +0100

    refactor/dynamic-programming

---
+++ src/reinforcement_learning_course/dynamic_programming/agents/taxi.py
@@ -0,0 +1,47 @@
+import argparse
+import gymnasium as gym
+from reinforcement_learning_course.dynamic_programming import algorithms as algs
+from reinforcement_learning_course.dynamic_programming.utils import taxi_transition_matrices
+from time import time
+
+if __name__ == '__main__':
+    
+    argparser = argparse.ArgumentParser(description='Parse options')
+    
+    argparser.add_argument('--gamma', type=float, default=0.99, help="Discount factor of the MDP.")
+    argparser.add_argument('--algorithm', type=str, choices=['value_iteration', 'q_iteration', 'policy_iteration'], default='value_iteration', help="Dynamic Programming algorithm to use {value_iteration, q_iteration, policy_iteration}.")
+    argparser.add_argument('--epsilon', type=float, default=1e-12, help="Discount factor of the MDP.")
+    argparser.add_argument('--n_train', type=int, default=1000, help="Number of training episodes.")
+    argparser.add_argument('--n_test', type=int, default=5, help="Number of test episodes.")
+    argparser.add_argument('--verbose', type=int, default=1, help="Whether to print each episode evaluation during the test phase.")
+    argparser.add_argument('--path_gif', type=str, help="Path where to save the test gif. If not provided, no gif save will be performed.")
+    
+    args = argparser.parse_args()
+
+    # Train
+    env = gym.make('Taxi-v3')
+    start_time = time()
+    if args.algorithm == 'value_iteration':
+        agent = algs.ValueIteration(env, taxi_transition_matrices, args.gamma)
+        
+    elif args.algorithm == 'q_iteration':
+        agent = algs.QIteration(env, taxi_transition_matrices, args.gamma)
+        
+    elif args.algorithm == 'policy_iteration':
+        agent = algs.PolicyIteration(env, taxi_transition_matrices, args.gamma)
+
+    agent.train(args.n_train, args.epsilon)
+    print('Execution time :', time() - start_time)
+    
+    # Test.
+    env = gym.make('Taxi-v3', render_mode='human')
+    agent.set_env(env)
+    agent.test(args.n_test, verbose=args.verbose)
+    env.close()
+    
+    # Save gif.
+    if args.path_gif is not None:
+        env = gym.make('Taxi-v3', render_mode='rgb_array')
+        agent.set_env(env)
+        agent.save_gif(args.path_gif, n_episodes=5, duration=150)
+        env.close()
---
+++ src/reinforcement_learning_course/dynamic_programming/utils.py
@@ -0,0 +1,249 @@
+import numpy as np
+from gymnasium.core import Env
+
+
+def frozen_lake_transition_matrices(env: Env[int, int]) -> tuple[np.array, np.array, int, int]:
+    """
+    Description
+    ------------------------------
+    Generate the probability and reward transition matrices.
+
+    Parameters
+    ------------------------------
+    env : Env, a frozen lake gymnasium environment
+
+    Returns
+    ------------------------------
+    p_transition : np.array of shape (n_state, n_actions, n_states), the transition probabilities matrix.
+    r_transition : np.array of shape (n_state, n_actions, n_states), the transition rewards matrix.
+    """
+
+    n_states, n_actions = env.observation_space.n, env.action_space.n
+    map = env.unwrapped.desc.astype(str)
+    shape = map.shape
+
+    p_transition, r_transition = np.zeros((n_states, n_actions, n_states)), np.zeros((n_states, n_actions, n_states))
+    for state in range(n_states):
+        state_index = np.unravel_index(state, shape)
+        # Holes are absorbing states.
+        if map[state_index] == 'H':
+            p_transition[state, :, state] = 1
+            
+        else:
+            for action in range(n_actions):
+                if action == 0: # Go left.
+                    # Going left.
+                    next_state_index = np.array(state_index)
+                    next_state_index[1] = max(0, next_state_index[1] - 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Slipping up.
+                    next_state_index = np.array(state_index)
+                    next_state_index[0] = max(0, next_state_index[0] - 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Slipping down.
+                    next_state_index = np.array(state_index)
+                    next_state_index[0] = min(shape[0] - 1, next_state_index[0] + 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                elif action == 2: # Go right.
+                    # Going right.
+                    next_state_index = np.array(state_index)
+                    next_state_index[1] = min(shape[1] - 1, next_state_index[1] + 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Slipping up.
+                    next_state_index = np.array(state_index)
+                    next_state_index[0] = max(0, next_state_index[0] - 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Slipping down.
+                    next_state_index = np.array(state_index)
+                    next_state_index[0] = min(shape[0] - 1, next_state_index[0] + 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                elif action == 1: # Go down.
+                    # Slipping left.
+                    next_state_index = np.array(state_index)
+                    next_state_index[1] = max(0, next_state_index[1] - 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Going down.
+                    next_state_index = np.array(state_index)
+                    next_state_index[0] = min(shape[0] - 1, next_state_index[0] + 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Slipping right.
+                    next_state_index = np.array(state_index)
+                    next_state_index[1] = min(shape[1] - 1, next_state_index[1] + 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                elif action == 3: # Go up.
+                    # Slipping left.
+                    next_state_index = np.array(state_index)
+                    next_state_index[1] = max(0, next_state_index[1] - 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Going up.
+                    next_state_index = np.array(state_index)
+                    next_state_index[0] = max(0, next_state_index[0] - 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+
+                    # Slipping right.
+                    next_state_index = np.array(state_index)
+                    next_state_index[1] = min(shape[1] - 1, next_state_index[1] + 1)
+                    next_state = next_state_index[0]*shape[0] + next_state_index[1]
+                    p_transition[state, action, next_state] += 1/3
+            
+                for next_state in range(n_states):
+                    next_state_index = np.unravel_index(next_state, shape)
+                    if map[next_state_index] == 'G':
+                        r_transition[state, action, next_state] = 1
+
+    return p_transition, r_transition, n_states, n_actions
+
+
+def cliff_walking_transition_matrices(env: Env[int, int]) -> tuple[np.array, np.array, int, int]:
+    """
+    Description
+    ------------------------------
+    Generate the probability and reward transition matrices.
+
+    Parameters
+    ------------------------------
+    env : Env, a cliff walking gymnasium environment
+
+    Returns
+    ------------------------------
+    p_transition : np.array of shape (n_state, n_actions, n_states), the transition probabilities matrix.
+    r_transition : np.array of shape (n_state, n_actions, n_states), the transition rewards matrix.
+    """
+    
+    n_states, n_actions = env.observation_space.n, env.action_space.n
+    shape = (4, 12)
+
+    p_transition, r_transition = np.zeros((n_states, n_actions, n_states)), np.full((n_states, n_actions, n_states), -1)
+    for state in range(n_states):
+        state_index = np.unravel_index(state, shape)
+        # Going over the cliff sends us back immediately to the initial state.
+        if state_index[0] == 3 and state_index[1] in set(range(1, 11)):
+            p_transition[state, :, 36] = 1
+            
+        # The goal state is an absorbing state.
+        elif state_index == (3, 11):
+            p_transition[state, :, state] = 1
+            r_transition[state, :, :] = 0
+
+        else:
+            for action in range(n_actions):
+                if action == 0: # Go Up.
+                    next_state_index = (max(0, state_index[0] - 1), state_index[1])
+                    next_state = next_state_index[0]*shape[1] + next_state_index[1]
+                    p_transition[state, action, next_state] = 1
+
+                elif action == 1: # Go right.
+                    next_state_index = (state_index[0], min(11, state_index[1] + 1))
+                    next_state = next_state_index[0]*shape[1] + next_state_index[1]
+                    p_transition[state, action, next_state] = 1
+
+                elif action == 2: # Go down.
+                    next_state_index = (min(3, state_index[0] + 1), state_index[1])
+                    next_state = next_state_index[0]*shape[1] + next_state_index[1]
+                    p_transition[state, action, next_state] = 1
+
+                elif action == 3: # Go left.
+                    next_state_index = (state_index[0], max(0, state_index[1] - 1))
+                    next_state = next_state_index[0]*shape[1] + next_state_index[1]
+                    p_transition[state, action, next_state] = 1
+            
+                for next_state in range(n_states):
+                    next_state_index = np.unravel_index(next_state, shape)
+                    # Going over the cliff incurs -100 reward.
+                    if next_state_index[0] == 3 and next_state_index[1] in set(range(1, 11)):
+                        r_transition[state, action, next_state] = -100
+                    
+    return p_transition, r_transition, n_states, n_actions
+
+
+def taxi_transition_matrices(env: Env[int, int]) -> tuple[np.array, np.array, int, int]:
+    """
+    Description
+    ------------------------------
+    Generate the probability and reward transition matrices.
+
+    Parameters
+    ------------------------------
+    env : Env, a taxi gymnasium environment
+
+    Returns
+    ------------------------------
+    p_transition : np.array of shape (n_state, n_actions, n_states), the transition probabilities matrix.
+    r_transition : np.array of shape (n_state, n_actions, n_states), the transition rewards matrix.
+    """
+
+    n_states, n_actions = env.observation_space.n + 1, env.action_space.n
+
+    p_transition, r_transition = np.zeros((n_states, n_actions, n_states)), np.zeros((n_states, n_actions, n_states))
+    for state in range(n_states):
+        # n_states-1 is the absorbing state.
+        if state == n_states - 1:
+            p_transition[state, :, state] = 1
+            
+        else:
+            taxi_row, taxi_col, passenger_loc, destination = env.unwrapped.decode(state)
+            action_mask = env.unwrapped.action_mask(state)
+            actions_allowed = np.arange(n_actions)[action_mask == 1]
+            for action in actions_allowed:
+                if action == 0: # Go south.
+                    taxi_row_new, taxi_col_new, passenger_loc_new = taxi_row + 1, taxi_col, passenger_loc
+                    next_state = env.unwrapped.encode(taxi_row_new, taxi_col_new, passenger_loc_new, destination)
+
+                    p_transition[state, action, next_state] = 1
+                    r_transition[state, action, next_state] = -1
+
+                elif action == 1: # Go north.
+                    taxi_row_new, taxi_col_new, passenger_loc_new = taxi_row - 1, taxi_col, passenger_loc
+                    next_state = env.unwrapped.encode(taxi_row_new, taxi_col_new, passenger_loc_new, destination)
+                    p_transition[state, action, next_state] = 1
+                    r_transition[state, action, next_state] = -1
+
+                elif action == 2: # Go east.
+                    taxi_row_new, taxi_col_new, passenger_loc_new = taxi_row, taxi_col + 1, passenger_loc
+                    next_state = env.unwrapped.encode(taxi_row_new, taxi_col_new, passenger_loc_new, destination)
+                    p_transition[state, action, next_state] = 1
+                    r_transition[state, action, next_state] = -1
+
+                elif action == 3: # Go west.
+                    taxi_row_new, taxi_col_new, passenger_loc_new = taxi_row, taxi_col - 1, passenger_loc
+                    next_state = env.unwrapped.encode(taxi_row_new, taxi_col_new, passenger_loc_new, destination)
+                    p_transition[state, action, next_state] = 1
+                    r_transition[state, action, next_state] = -1
+
+                elif action == 4: # Pickup passenger.
+                    taxi_row_new, taxi_col_new, passenger_loc_new = taxi_row, taxi_col, 4
+                    next_state = env.unwrapped.encode(taxi_row_new, taxi_col_new, passenger_loc_new, destination)
+                    p_transition[state, action, next_state] = 1
+                    r_transition[state, action, next_state] = -1
+
+                elif action == 5: # Drop off passenger and transition to the absorbing state.
+                    p_transition[state, action, n_states - 1] = 1
+                    # If the passenger is dropped at the correct destination, incur a reward of 20. Otherwise incur a reward of -10.
+                    if (taxi_row, taxi_col) == env.unwrapped.locs[destination]:
+                        r_transition[state, action, n_states - 1] = 20
+
+                    else:
+                        r_transition[state, action, n_states - 1] = -10
+                    
+    return p_transition, r_transition, n_states, n_actions
